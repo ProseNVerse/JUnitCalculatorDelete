f4a1d2b20f58128be7eda9d039ecf20288036426
ProseNVerse <walker.atlas@yahoo.com>
2018-05-15 10:41:56 +0100

    init commit

---
+++ src/Calculator/Calc.java
@@ -0,0 +1,17 @@
+package Calculator;
+
+public class Calc {
+
+    public int addition(int a, int b){
+        return a+b;
+    }
+    public int subtraction (int a, int b){
+        return a - b;
+    }
+    public int multiplication (int a, int b){
+        return a * b;
+    }
+    public int division (int a, int b){
+        return a / b;
+    }
+}
